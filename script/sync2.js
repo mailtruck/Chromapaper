fe0f8d58cb114d1c78455002fffe31505634acc4
ThomasApps <thomas@mream.net>
2011-02-28 20:05:24 -0500

    The rewrite continues! Next up: actually scraping and saving the pages.

--- script/sync2.js
+++ script/sync2.js
@@ -9,37 +9,54 @@ Split into several namespaces to cut down on globals:
 		.urls - scrapes & returns all the urls on the page
 		.details
 	
-	articleScraper.*:
+	pageScraper.*:
 		
 		
 	dbCallbacks.*:
 		
 	overlays.*:	
 		.sync
-		.noArticles
+		.noPages
 		.notLoggedIn
 */
 
-var sync = {
-	var urls;
-	var details;
-	
-	function start() {
-		overlay.sync();
+var options = new function() {
+	var saveImagesOption = localStorage['saveImagesOn']
+	if (!saveImagesOption) {
+		saveImagesOption = "false";
+	}
 
-		urls = instapaperScraper.getUrls();
-		if (urls == "no articles saved") {
-			overlay.noArticles();
+	var folders_to_sync = localStorage['folders'];
+	console.log(folders_to_sync);
+	if (folders_to_sync) {
+		this.folders_to_sync = folders_to_sync.split(','); //gahhhhh
+	}
+	else {
+		this.folders_to_sync = ""
+	}
+}
+
+var sync = new function() {
+	//this.urls;
+	//this.details;
+	
+	this.start = function() {
+		//overlay.sync();
+
+		this.pages = instapaperScraper.getListHtml();
+		if (this.pages == "no pages saved") {
+			overlay.noPages();
 			return;
 		}
-		else if (urls == "not logged in") {
+		else if (this.pages == "not logged in") {
 			overlay.notLoggedIn();
 			return;
 		}
-		details = instapaperScraper.getDetails(urls);
+		this.urls = instapaperScraper.getUrls(this.pages);
+		this.details = instapaperScraper.getDetails(this.pages);
 		
 		dbSync.start();
-	}
+	};
 	
 }
 
@@ -48,40 +65,56 @@ function pagesLoaded() {
 	sync.start();
 }
 
-/* dbSync
-articles_to_save
-articles_to_archive
-articles_saved
-articles_archived
-images_to_save?
+var syncProgress = new function() {
+	this.url_check_done = false;
+	this.saves_done = false;
+	this.archives_done = false;
+	this.images_done = false;
+	
+	this.check = function() {
+		if (this.saves_done == true && this.archives_done == true && this.images_done == true) {
+			console.log("all done!!");
+		}
+	}
+}
 
-----synchronous
-for each page
-	check if saved-> call back saveArticle
-----
-save article */
-
-var dbSync = {
-	var articles_to_save;
-	var articles_to_archive;
+var dbSync = new function() {
+	this.pages_to_save = new Array();
+	var pages_to_archive;
 	
-	var articles_saved;
-	var articles_archived;
+	var status = 'continue';
 	
-	var status = 'continue'
-	
-	function start() {
-		for (url in sync.urls) {
+	this.start = function() {
+		for (i in sync.urls) {
 			//this basically queues each query to be acted on once the loop is complete...
-			while (dbSync.status == 'continue') {
-				dbSync.checkIn(sync.urls[url]);
-			}
+			checkIn(i);
+			console.log("in da loop??");
 		}
 		
-		dbSync.checkArchive();
+		console.log("done savin'???");
+		
+		//dbSync.checkArchive();
+	};
+	var checkIn = function (i) {
+		database.db.transaction(function(tx) {
+			tx.executeSql("select * from pages where url=?",
+				[sync.urls[i]],
+				function (tx, results) {
+					if (results.rows.length == 0) {
+						console.log("maybe saving page??? " + sync.urls[i]);
+						page = scrapePage(sync.urls[i], i); //this may work?? idk
+						dbSync.pages_to_save.push(page);
+					}
+					if (i == (sync.urls.length - 1)) {
+						console.log("hey hit the last one!!");
+						syncProgress.url_check_done = true;
+						scrapePages(pages_to_save);
+					}
+				},
+				database.onError
+			);
+		});
 	}
-	
-	
 }
 
 
@@ -92,64 +125,100 @@ var dbSync = {
 
 */
 
-var instapaperScraper = {
+var instapaperScraper = new function() {
 	var instapaper_html = new Array();
-	function getUrls() {
+	this.getListHtml = function() {
 		var page_loop_counter = 1;
 		
 		var status = "continue";
 		//loop this for each page
-		for (page_loop_counter = 1; status == "continue"; page_loop_counter++) {
-			status = instapaperScraper.scrapePage("http://instapaper.com/u/" + (instapaper_html.length + 1)); //this is a little weird... scrape page automatically adds it to instapaper_html, so we return the status to decide if we should keep looping
+		 while (status == "continue") {
+			status = scrapePage("http://www.instapaper.com/u/" + (instapaper_html.length + 1)); //this is a little weird... scrape page automatically adds it to instapaper_html, so we return the status to decide if we should keep looping
+			console.log(status);
 		}
 		if (status != 'done') {
-			return status; //means not logged in or no articles
+			return status; //means not logged in or no pages
 		}		
 	
+		//todo: what if user has no articles in unread but has articles in folders??
 	
-		if (foldersToSync) {
-			for (i in foldersToSync) {
-				folderNum = foldersToSync[i];
+		if (options.folders_to_sync != "") {
+			for (i in options.folders_to_sync) {
+				folderNum = options.folders_to_sync[i];
 				page_loop_counter = 1;
 			
-				status = "continue";
+				var status = "continue";
 				while (status == "continue") {
-					status = instapaperScraper.scrapePage("http://instapaper.com/u/folders" + folderNum + "/fakename/" + page_loop_counter, false);
-					page_loop_counter++
+					status = scrapePage("http://www.instapaper.com/u/folder/" + folderNum + "/fakename/" + page_loop_counter, false);
+					page_loop_counter++;
 				}
 			}
 		}
-	
+		
 		return instapaper_html;
 	}
-	function scrapePage(url) {	
-		var instapaperPage = new XMLHttpRequest();
+	var scrapePage = function(url) {	
+		console.log(url);
+		var instapaper_page = new XMLHttpRequest();
 		try {
-			instapaperPage.open("GET", url, false);
-			instapaperPage.send();
+			instapaper_page.open("GET", url, false);
+			instapaper_page.send();
 		}
 		catch(err) {
-			console.log("Error description: " + err.description);
+			console.log("Error description: " + err);
 		}
 	
-		var instapaper_page_html_string = instapaperPage.responseText;
-
+		var instapaper_page_html_string = new String();
+		var instapaper_page_html_string = instapaper_page.responseText;
+		
 		if (instapaper_page_html_string.search("Log out") == -1) {
+			console.log("ain't logged in bub");
 			status = "not logged in";
 		} 
-		else if (instapaper_page_html_string.search("No articles saved.") == -1 && instapaper_html.length == 0) {
-			status = "no articles saved";
+		else if (instapaper_page_html_string.search("No articles saved.") != -1 && instapaper_html.length == 0) {
+			console.log("ain't got no saved pages");
+			status = "no pages saved";
 		} 
-		else if (instapaper_page_html_string.search("No articles saved.") != -1) {
-			//no articles saved *on this page* so we're done!
+		else if (instapaper_page_html_string.search("No articles saved.") != -1 || instapaper_page_html_string.search("No articles in this folder.") != -1) {
+			//no pages saved *on this page* so we're done!
+			console.log("all done!");
 			status = "done";
 		}
 		else {
+			status = "continue";
 			instapaper_html.push(instapaper_page_html_string);                                    
 		}
 		return status;
 	}
-	function getDetails(instapaperPagesHTML) {
+	this.getUrls = function(instapaper_html) {
+		var instapaperURLs = new Array();
+
+		var loop_counter = 1;
+		var num_urls = 0;
+
+		for (loop_counter in instapaper_html) {
+			var instapaperPageStringUnescaped = instapaper_html[loop_counter];
+			var instapaperPageString = instapaperPageStringUnescaped.replace(/&/gi, "&amp;");
+
+			parser = new DOMParser();
+
+			instapaperPage = parser.parseFromString(instapaperPageString,"text/xml");
+
+			links = instapaperPage.getElementsByTagName("a");
+
+			for (i=0;i<links.length;i++) {
+				url = links[i].getAttribute("href");
+				if (url.startsWith("/text") == true || url.startsWith("/go") == true) {
+					instapaperURLs[num_urls] = url;
+					num_urls++;
+				}
+			}
+		}
+		instapaperURLs.reverse();
+		return instapaperURLs;
+	}
+	
+	this.getDetails = function(instapaperPagesHTML) {
 	
 		//this array holds the details objects for each page
 		var instapaperDetails = new Array();
@@ -166,9 +235,9 @@ var instapaperScraper = {
 			instapaperPageString = instapaperPageString.replace("<br/<br/>","<br/><br/>")
 		
 			parser = new DOMParser();
-		
+			
 			instapaperPage = parser.parseFromString(instapaperPageString,"text/xml");
-		
+			
 			allDivs = instapaperPage.getElementsByTagName("div");
 		
 			//traverse until we find the bookmark list
@@ -210,10 +279,14 @@ var instapaperScraper = {
 					}
 				}
 			}
-		
 		}
+		
 		instapaperDetails.reverse();
 	
 		return instapaperDetails;
 	}
+}
+
+function scrapePages(pages_to_save) {
+	
 }
